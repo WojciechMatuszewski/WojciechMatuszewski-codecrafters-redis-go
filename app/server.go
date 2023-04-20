995e0e9c7cec2f711418f64e44f8005d78c36800
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2023-04-20 05:16:15 +0200

    try

--- app/server.go
+++ app/server.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
 )
 
+const (
+	HOST = "0.0.0.0"
+	PORT = "6379"
+)
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%s", HOST, PORT))
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	var buf bytes.Buffer
+	_, err = buf.ReadFrom(conn)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	conn.Close()
 }
